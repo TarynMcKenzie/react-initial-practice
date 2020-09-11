11f9f92a8878e1df95fe1347ae933f1082d40ef0
GPS <gps@GPSs-MBP.satx.rr.com>
2020-09-11 11:33:20 -0500

    Initial tutorial video completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-const http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200);
-  response.write('<h1>Hello Node!!!!</h1>\n');
-  response.end();
-}).listen(3000);
-console.log('Server running at http://localhost:3000');
+// const http = require('http');
+// http.createServer(function (request, response) {
+//   response.writeHead(200);
+//   response.write('<h1>Hello Node!!!!</h1>\n');
+//   response.end();
+// }).listen(3000);
+// console.log('Server running at http://localhost:3000');
+
+
+import express from 'express';
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello Worl!!');
+});
+
+app.listen(3000, function (){
+    console.log('Example app listening on port 3000')
+})
