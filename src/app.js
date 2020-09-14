a38a70058600a18f1e9cfc874079ce0c28b2b937
GPS <taryn.m.mckenzie@gmail.com>
2020-09-14 09:14:38 -0500

    Finished setup for React project.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import React from 'react'; // Look at the react files int he node_modules folder
+import "app.css"; // Look at the css file to get style
+// import image from "image.jpg"; // Look at the image file to load image
+
+
+// ======= Component Imports =======
+import Nav from "./nav"; // Look at th nav.js file for the navigation
 
 // -----OLDER WAY TO RENDER REACT-----
 // const app = () => {
@@ -11,7 +17,9 @@ import React from 'react'; // Look at the react files int he node_modules folder
 // -----NEWER WAY TO RENDER REACT-----
 const App = () => ( // Will implicity return the html elements
     <div>
-        <h1>Hello React</h1>
+        <Nav />
+        <h1 className="main_header">Hello React</h1>
+        {/* <img src={image} alt="" /> */}
     </div>
 );
 
---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from 'React';
+
+const nav = () => (
+    
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </nav>
+
+);
+
+export default nav;
